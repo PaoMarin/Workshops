96e5918003d7d1c81b1367f66c600385b898e59a
PaoMarin <paomg.9614@gmail.com>
2018-09-18 20:28:44 -0600

    Whorkshop1

---
+++ helloworld.php
@@ -0,0 +1,15 @@
+
+<html>
+ <head>
+  <title>Hello World PHP</title>
+ </head>
+ <body>
+<?php 
+$value = "Hello World";
+
+$image ="http://fayimora.com/content/images/2015/01/hello_world.gif";
+echo $value;
+echo "<img src= $image>";
+?>
+ </body>
+</html>
